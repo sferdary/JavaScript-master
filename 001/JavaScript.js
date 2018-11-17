0caeb7e0521ef6bf336eef0afb541febbe7b9f01
sferdary <s.ferdary@pm.me>
2018-11-17 21:30:34 +0100

    JavaScript exercises

---
+++ 001/JavaScript.js
@@ -0,0 +1,14 @@
+for (let i = 0; i<10; i++){
+    console.log("i has the value of " + i);
+}
+
+const car = {
+    brand: "Ford",
+    type: "Mondeo",
+    numberOfWheels: 4,
+    color: "blue"
+};
+console.log("Car brand:", car.brand,);
+console.log("type of car:", car.type,);
+console.log("Number of wheels:", car.numberOfWheels,);
+console.log("Color of car:", car.color,);
