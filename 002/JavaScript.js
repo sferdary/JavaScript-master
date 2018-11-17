ab0c66201924ffce1cc4c9ebbc6af27ca7392dbe
sferdary <s.ferdary@pm.me>
2018-11-17 22:53:03 +0100

    JavaScript exercises

---
+++ 002/JavaScript.js
@@ -0,0 +1,23 @@
+document.getElementById("title").innerText = "Page title";
+document.getElementById("subtitle").innerText = "Sub title";
+
+document.getElementById("monkey01").src = "img/monkey01.jpg";
+document.getElementById("monkey02").src = "img/monkey02.jpg";
+
+document.getElementById("bl1").style.backgroundColor = "blue";
+document.getElementById("bl1").style.marginTop = "20px";
+document.getElementById("bl1").style.textAlign = "center";
+document.getElementById("text01").style.marginTop = "20px";
+
+document.getElementById("bl2").style.backgroundColor = "green";
+document.getElementById("bl2").style.marginTop = "40px";
+document.getElementById("bl2").style.textAlign = "center";
+document.getElementById("text02").style.marginTop = "0px";
+
+document.getElementById("bl3").style.backgroundColor = "yellow";
+document.getElementById("bl3").style.marginTop = "60px";
+document.getElementById("bl3").style.textAlign = "right";
+
+document.getElementById("bl4").style.backgroundColor = "red";
+document.getElementById("bl4").style.marginTop = "20px";
+document.getElementById("text04").style.marginTop = "0px";
